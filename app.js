98b7491c6174df3dc2b97c1da7bd86712fd7fad2
Rajan Kumar <rajan847104@gmail.com>
2026-02-03 20:24:02 +0530

    authentication demo

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport")
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const ExpressError = require("./utils/ExpressError.js");
 
@@ -53,11 +56,30 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
    res.locals.success = req.flash("success");
    res.locals.error = req.flash("error");
    next();
 });
+
+app.get("/demouser", async (req, res) => {
+    let fakeUser = new User({
+        email: "rajan@gmail.com",
+        username: "sigma-student",
+    });
+
+   let registerUser = await User.register(fakeUser, "password");
+   res.send(registerUser);
+
+});
+
 
 
 app.use("/listings", listings);
