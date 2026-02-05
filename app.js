3bda3693ae874bd3e4b3d966867e232bc40ebd58
Rajan Kumar <rajan847104@gmail.com>
2026-02-05 22:43:47 +0530

    Signup and Login

--- app.js
+++ app.js
@@ -13,8 +13,9 @@ const User = require("./models/user.js");
 const ExpressError = require("./utils/ExpressError.js");
 
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRuoter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -69,21 +70,22 @@ app.use((req, res, next) => {
    next();
 });
 
-app.get("/demouser", async (req, res) => {
-    let fakeUser = new User({
-        email: "rajan@gmail.com",
-        username: "sigma-student",
-    });
+// app.get("/demouser", async (req, res) => {
+//     let fakeUser = new User({
+//         email: "rajan@gmail.com",
+//         username: "sigma-student",
+//     });
 
-   let registerUser = await User.register(fakeUser, "password");
-   res.send(registerUser);
+//    let registerUser = await User.register(fakeUser, "password");
+//    res.send(registerUser);
 
-});
+// });
 
 
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.use("/listings", listingRuoter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 
 app.use((req, res, next) => {
